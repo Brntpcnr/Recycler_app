936009c56f95e63fc7212117efca5616b7112430
Brntpcnr <brntpcnr@gmail.com>
2023-07-04 13:17:21 +0300

    Recycle App Kusadasi Containers

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -16,7 +16,7 @@ class _MapsState extends State<Maps> {
   Set<Marker> markers = {
     Marker(
       markerId: MarkerId('marker_1'),
-      position: LatLng(35.1457, 33.9071), // Coordinates for marker location
+      position: LatLng(37.860593293843216, 27.259611567311957), // Coordinates for marker location
       infoWindow: InfoWindow(
         title: 'Container Area',
         snippet: 'Glass Container',
@@ -24,7 +24,7 @@ class _MapsState extends State<Maps> {
     ),
     Marker(
       markerId: MarkerId('marker_2'),
-      position: LatLng(35.141698818730546, 33.90217148488927), // Coordinates for marker location
+      position: LatLng(37.85812100092891, 27.257628848512564), // Coordinates for marker location
       infoWindow: InfoWindow(
         title: 'Container Area',
         snippet: 'Metal Container',
@@ -32,7 +32,7 @@ class _MapsState extends State<Maps> {
     ),
     Marker(
       markerId: MarkerId('marker_3'),
-      position: LatLng(35.1450652238407, 33.90509910700379), // Coordinates for marker location
+      position: LatLng(37.85921490221663, 27.258390399598646), // Coordinates for marker location
       infoWindow: InfoWindow(
         title: 'Container Area',
         snippet: 'Plastic Container',
